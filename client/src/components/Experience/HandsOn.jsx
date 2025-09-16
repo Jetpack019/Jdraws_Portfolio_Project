feaaa6a0b34565b46c381b2f737d1578bca5dd07
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2025-09-17 07:09:44 +0800

    Hands On Experience Finished

--- client/src/components/Experience/HandsOn.jsx
+++ client/src/components/Experience/HandsOn.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProjectList from "../Homepage/ProjectList";
+import Designs from "./Designs";
 
 function HandsOn() {
   return (
     <>
       <ProjectList />
+      <Designs />
     </>
   );
 }
